df37bacc8a2be2065f1adefbd3b50dab5d1e4575
tasos <lazarou.ts@gmail.com>
2021-07-15 13:16:06 +0300

    addAwsomeFonts

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -22,8 +22,16 @@
     <tr>
       <th>Steal bananas from the store.</th>
       <td>To-do</td>
-      <td></td>
-      <td></td>
+      <td>
+          <div class="text-center">
+            <span class="fa fa-pen"></span>
+          </div>
+     </td>
+      <td>
+          <div class="text-center">
+            <span class="fa fa-trash"></span>
+          </div>
+      </td>
     </tr>
   </tbody>
 </table>
